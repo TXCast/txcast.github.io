e56ebef0c671970fbcc969b92394cf462d711e6a
OFRBG <fi@SaturnMx.local>
2018-12-10 01:07:19 -0600

    Updates

---
+++ precache-manifest.2f998d30d8b8199879a430ad6f6d52ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "21f3140403150753c2b25ff450031712",
+    "url": "/static/media/logo.21f31404.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b1ab3f48a1b3a7b101cd",
+    "url": "/static/js/main.b1ab3f48.chunk.js"
+  },
+  {
+    "revision": "eee0029559ec3a9a9fb0",
+    "url": "/static/js/1.eee00295.chunk.js"
+  },
+  {
+    "revision": "b1ab3f48a1b3a7b101cd",
+    "url": "/static/css/main.75680a79.chunk.css"
+  },
+  {
+    "revision": "9953828ed9f747d22a95758981bcb4c7",
+    "url": "/index.html"
+  }
+];
